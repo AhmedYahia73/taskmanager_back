e21490f6d603705d5a19f7e08a12cbc0bd046c2c
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-08-09 16:42:13 +0300

    commit

--- dist/src/routes/user/notesBoard.js
+++ dist/src/routes/user/notesBoard.js
@@ -5,7 +5,10 @@ const notesBoard_1 = require("../../controllers/user/notesBoard");
 const notesBoard_2 = require("../../controllers/user/notesBoard");
 const catchAsync_1 = require("../../utils/catchAsync");
 const validation_1 = require("../../middlewares/validation");
+const authenticated_1 = require("../../middlewares/authenticated");
 const route = (0, express_1.Router)();
+// Apply auth middleware
+route.use(authenticated_1.authenticated);
 // Get notes by group id (Engineers)
 route.get("/group/:groupId", (0, validation_1.validate)(notesBoard_2.GroupIdSchema), (0, catchAsync_1.catchAsync)(notesBoard_1.getNotesByGroupId));
 exports.default = route;
--- src/routes/user/notesBoard.ts
+++ src/routes/user/notesBoard.ts
@@ -3,8 +3,12 @@ import { getNotesByGroupId } from "../../controllers/user/notesBoard";
 import { GroupIdSchema } from "../../controllers/user/notesBoard";
 import { catchAsync } from "../../utils/catchAsync";
 import { validate } from "../../middlewares/validation";
+import { authenticated } from "../../middlewares/authenticated";
 
 const route = Router();
+
+// Apply auth middleware
+route.use(authenticated);
 
 // Get notes by group id (Engineers)
 route.get("/group/:groupId", validate(GroupIdSchema), catchAsync(getNotesByGroupId));
